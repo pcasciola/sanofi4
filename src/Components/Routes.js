2e1175b4b845f10e36445b57dddc85ea89c08dda
pcasciola <pcasciola@gmail.com>
2022-03-12 14:08:33 -0500

    Updated major versions of react-router and react-ga Router changed Switch to Routes, and component to element

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Scheduler from "./Scheduler";
 import Introduction from "./Introduction";
@@ -8,11 +8,11 @@ import Disclaimer from "./Disclaimer";
 
 export default () =>
     (<BrowserRouter hosted-basename={"/"}>
-            <Switch>
-                <Route exact path="/" component={Disclaimer} />
-                <Route path="/introduction" component={Introduction} />
-                <Route path="/login" component={LogIn} />
-                <Route path="/scheduler" component={Scheduler} />
-            </Switch>
+            <Routes>
+                <Route exact path="/" element={<Disclaimer/>} />
+                <Route path="/introduction" element={<Introduction/>} />
+                <Route path="/login" element={<LogIn/>} />
+                <Route path="/scheduler" element={<Scheduler/>} />
+            </Routes>
         </BrowserRouter>
     );
